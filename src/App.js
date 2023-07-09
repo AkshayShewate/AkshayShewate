fe179c16ddb266115538e2b0241b8cbb8f4d80bf
Akshay Shewate <aksshewate779@gmail.com>
2023-07-09 16:20:09 +0530

    Add new image react app code and deploy on Akshay Shewate repos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { FacebookShareButton, TwitterShareButton, WhatsappShareButton } from 'react-share';
+
+function getRandomImage() {
+  const imageUrl = [
+    'https://res-4.cloudinary.com/demo/image/upload/sample.jpg',
+    // Add more image URLs here
+  ];
+  const randomIndex = Math.floor(Math.random() * imageUrl.length);
+  return imageUrl[randomIndex];
+}
 
 function App() {
+  const [image, setImage] = useState('');
+
+  useEffect(() => {
+    const randomImage = getRandomImage();
+    setImage(randomImage);
+  }, []);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="App" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div style={{ textAlign: 'center' }}>
+        <h1>HQ Image Application</h1>
+        {image && <img src={image} alt="Random" style={{ maxWidth: '100%', height: 'auto' }} />}
+        <div style={{ textAlign: 'center', marginTop: '20px' }}>
+            <FacebookShareButton url={image} style={{ backgroundColor: 'blue', padding:'10px', margin:'10px' }} >
+              Share on Facebook
+            </FacebookShareButton>
+            <TwitterShareButton url={image}  style={{ backgroundColor: 'red', padding:'10px', margin:'10px' }} >
+              Share on Twitter
+            </TwitterShareButton>
+            <WhatsappShareButton url={image}  style={{ backgroundColor: 'green', padding:'10px', margin:'10px' }}>
+              Share on WhatsApp
+            </WhatsappShareButton>
+          </div>
+        
+      </div>
     </div>
+    </>
   );
 }
 
